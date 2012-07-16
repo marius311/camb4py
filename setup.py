4702eb43db5205549f434f83fcba2b0a3801b5ea
marius <mmillea@ucdavis.edu>
2012-07-15 19:01:34 -0700

    cleaned up setup, openmp hack

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from numpy.distutils.command.build import build as _build
 from numpy.distutils.core import setup
-from numpy.distutils.fcompiler import new_fcompiler
+import numpy.distutils.fcompiler as FC
 from distutils.errors import DistutilsError
 
 
@@ -34,36 +34,62 @@ class build(_build):
         _build.initialize_options(self)
         self.no_builtin = None
         
+        
+    def get_openmp_flags(self, fcompiler):
+        """Hack to get OpenMP flags. Only gnu and intel will works, other are single threaded."""
+        fc_name = {v:k for (k,(_,v,_)) in FC.fcompiler_class.items()}.get(fcompiler.__class__)
+        if fc_name is not None:
+            if fc_name.startswith('gnu'):
+                return ['-fopenmp']
+            elif fc_name.startswiths('intel'):
+                return ['-openmp']
+        return []
+        
+        
+    def get_fcompiler(self):
+        """Get an fcompiler (including some hacks) or print error message if not found."""
+        fcompiler = FC.new_fcompiler(compiler=self.fcompiler,
+                                     verbose=self.verbose,
+                                     dry_run=self.dry_run,
+                                     force=self.force,
+                                     c_compiler=self.compiler)
+        
+        if fcompiler is None:
+            raise DistutilsError('Could not find Fortran compiler. See setup.cfg to specify one.')
+            
+        fcompiler.customize(self.distribution)
+        fcompiler.customize_cmd(self)
+        fcompiler.show_customization()
+        
+        #Hack because sometimes the executable linker is missing
+        if not fcompiler.linker_exe: fcompiler.linker_exe = fcompiler.linker_so
+
+        return fcompiler
+
+        
     def run(self):
         _build.run(self)
         
         if not self.no_builtin and (self.force or not os.path.exists(os.path.join(self.build_lib,'pycamb','camb'))):
             
-            self.fcompiler = new_fcompiler(compiler=self.fcompiler,
-                                           verbose=self.verbose,
-                                           dry_run=self.dry_run,
-                                           force=self.force,
-                                           c_compiler=self.compiler)
+            fcompiler = self.get_fcompiler()
             
-            if self.fcompiler is None:
-                raise DistutilsError('Could not find Fortran compiler. See setup.cfg to specify one.')
-                
-            self.fcompiler.customize(self.distribution)
-            self.fcompiler.customize_cmd(self)
-            self.fcompiler.show_customization()
-                
             src_dir = os.path.join('pycamb','src')
 
             self.copy_file(os.path.join(src_dir,'HighLExtrapTemplate_lenspotentialCls.dat'), os.path.join(self.build_lib,'pycamb'))
-
-            #Hack because sometimes the executable linker is missing
-            if not self.fcompiler.linker_exe: self.fcompiler.linker_exe = self.fcompiler.linker_so
     
-            obj_files = self.fcompiler.compile([os.path.join(src_dir,'%s.f90'%o) for o in self.objs],
-                                               extra_postargs=['-cpp',self.fcompiler.module_dir_switch+os.path.join(self.build_temp,src_dir)],
-                                               output_dir=self.build_temp)
+            openmp_flags = self.get_openmp_flags(fcompiler)
+            compile_flags = openmp_flags + ['-cpp',fcompiler.module_dir_switch+os.path.join(self.build_temp,src_dir)]
+            link_flags = openmp_flags
+            
+            obj_files = fcompiler.compile([os.path.join(src_dir,'%s.f90'%o) for o in self.objs],
+                                           extra_postargs=compile_flags,
+                                           output_dir=self.build_temp)
                 
-            self.fcompiler.link_executable(obj_files,'camb',output_dir=os.path.join(self.build_lib,'pycamb'))
+            fcompiler.link_executable(obj_files,
+                                      'camb',
+                                      output_dir=os.path.join(self.build_lib,'pycamb'),
+                                      extra_postargs=link_flags)
 
         
         
